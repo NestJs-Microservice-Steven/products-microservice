04906e8c92673d26fe2b23774e1a61adb3d294fb
Steven Muñoz <darkmixt@gmail.com>
2024-04-08 16:39:46 -0600

    feat: env variables

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+
+	@IsString()
+	public name : string;
+
+	@IsNumber({ maxDecimalPlaces: 4,})
+	@Min(0)  // el numero minimo que puede aceptar es 0
+	@Type(() => Number) // intenta convertir el tipo string a un numero
+	public price: string;
+
+
+}
